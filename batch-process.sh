34ce636b1f6ae16df43a407f4a74bc260ba0abe1
W M Hill <123903924+w-m-hill@users.noreply.github.com>
2023-03-11 02:26:13 -0800

    Add files via upload

---
+++ batch-process.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the path to the directory containing the images
+dir_path="/home/xmbo/Projects/SampleFolder"
+
+# Iterate over each file in the directory
+for file in "$dir_path"/*; do
+    # Check if the file is a regular file (not a directory or symlink)
+    if [ -f "$file" ]; then
+        # Call the python script with the file path as an argument
+        python append-clip-interrogation.py --path "$file"
+    fi
+done
