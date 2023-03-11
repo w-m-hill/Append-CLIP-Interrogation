c1c9c161af5ee414a09318faa8e156732a2343e7
w-m-hill <jerrymirandahill@gmail.com>
2023-03-11 01:51:11 -0800

    Switch to using argparse in place of of sys.argv

--- append-clip-interrogation.py
+++ append-clip-interrogation.py
@@ -1,34 +1,48 @@
-from PIL import Image
 import sys
+import argparse
 import os
+
+from PIL import Image
 from clip_interrogator import Config, Interrogator
 
-# Define the constant for the image size
-IMAGE_SIZE = 1024
 
-if len(sys.argv) == 2:
-	imagePath = sys.argv[1]
+parser = argparse.ArgumentParser(description='Appened CLIP interrogation to file')
+
+parser.add_argument('--path', type=str, help='Path to the image')
+parser.add_argument('--crop', action='store_true', help='Pass a cropped version of the image to interrogator if it is larger than --res')
+parser.add_argument('--res', type=int, help='Cropped resolution', default=1024)
+
+args = parser.parse_args()
+
+if (args.path):
+	imagePath = args.path
 	if (not os.path.exists(imagePath)):
 		sys.exit("Error: Invalid path.")
 else:
-	sys.exit("Error: Please provide a path as a command line argument.")
+	sys.exit("Error: Please provide a path to the image using --path path/to/image.")
 
 image = Image.open(imagePath).convert('RGB')
 
+if (args.crop):
+	# Define the constant for the image size
+	IMAGE_SIZE = args.res
 
-# Get the original image size
-width, height = image.size
+	## Get the original image size
+	width, height = image.size
+	if (width > size or height > size):
+		# Calculate the coordinates for the center crop
+		left, top = (width - IMAGE_SIZE) // 2, (height - IMAGE_SIZE) // 2
 
-# Calculate the coordinates for the center crop
-left, top = (width - IMAGE_SIZE) // 2, (height - IMAGE_SIZE) // 2
+		# Crop the image to the specified size at the center
+		image = image.crop((left, top, left + IMAGE_SIZE, top + IMAGE_SIZE))
 
-# Crop the image to the specified size at the center
-cropped_image = image.crop((left, top, left + IMAGE_SIZE, top + IMAGE_SIZE))
-
+		print(f"Copy of image was cropped to {args.res} by {args.res}")
+	else:
+		print(f"Image was already smaller than supplied resolution of {args.res}")
 
 #Interrogate
 ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
-result_string = ci.interrogate(cropped_image)
+result_string = ci.interrogate(image)
 
 
 # get the base name of the file (i.e., the file name without the directory)
