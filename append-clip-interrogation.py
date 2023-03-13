5351e1fb5a7b4928b2efffe59e774743a9c628f0
W M Hill <123903924+w-m-hill@users.noreply.github.com>
2023-03-13 04:55:17 -0700

    Implement --replace option

--- append-clip-interrogation.py
+++ append-clip-interrogation.py
@@ -9,6 +9,8 @@ from clip_interrogator import Config, Interrogator
 parser = argparse.ArgumentParser(description='Appened CLIP interrogation to file')
 
 parser.add_argument('--path', type=str, help='Path to the image')
+parser.add_argument('--replace', action='store_true', help='Replace the original name with caption instead of appending to it')
+
 parser.add_argument('--crop', action='store_true', help='Pass a cropped version of the image to interrogator if it is larger than --res')
 parser.add_argument('--res', type=int, help='Cropped resolution', default=1024)
 
@@ -47,22 +49,28 @@ result_string = ci.interrogate(image)
 
 # get the base name of the file (i.e., the file name without the directory)
 base_name = os.path.basename(imagePath)
-
 # split the base name into its name and extension parts
 name, ext = os.path.splitext(base_name)
-
-# add caption to the end of the name part
-new_name = name + result_string
-
 # get the maximum length of a file name for the file system where the file is located
 max_name_length = os.pathconf(os.path.dirname(imagePath), 'PC_NAME_MAX')
 
-# truncate the new name if it is too long
-if len(new_name + ext) > max_name_length:
-    new_name = new_name[:max_name_length - len(ext)]
+if (args.replace):
+	# truncate the result_string if it is too long
+	if len(result_string + ext) > max_name_length:
+	    result_string = result_string[:max_name_length - len(ext)]
 
-# rebuild the path with the new file name and the original directory
-new_path = os.path.join(os.path.dirname(imagePath), new_name + ext)
+	# rebuild the path with the new file name and the original directory
+	new_path = os.path.join(os.path.dirname(imagePath), result_string + ext)
+else:
+	# add caption to the end of the name part
+	new_name = name + result_string
+
+	# truncate the new name if it is too long
+	if len(new_name + ext) > max_name_length:
+	    new_name = new_name[:max_name_length - len(ext)]
+
+	# rebuild the path with the new file name and the original directory
+	new_path = os.path.join(os.path.dirname(imagePath), new_name + ext)
 
 
 # rename the file
